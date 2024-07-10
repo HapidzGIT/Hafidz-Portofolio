d4fa5f870072909acc125ed06392a30adba6dd4c
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-07-10 19:06:06 +0700

    Deployed

--- components/footer.tsx
+++ components/footer.tsx
@@ -24,8 +24,8 @@ export const Footer = () => {
                     <ul className="font-medium tracking-[2px] underline">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
-                        <li><a href="#skills">Skills</a></li>
-                        <li><a href="#project">Project</a></li>
+                        <li><a href="#skils">Skills</a></li>
+                        <li><a href="#projects">Project</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
